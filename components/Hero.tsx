dce6961ab70e45fc6aff0826354d502e0686e93a
n-alex-goncalves <goncalvesnuno01@gmail.com>
2025-01-02 11:25:04 +0000

    Added an arrow to the hero page and updated CV

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,18 +3,22 @@ import { ArrowIcon } from './Icon'
 
 const Hero = () => {
   return (
-    <div className="hero min-h-screen min-w-full">
-    <div className="hero-content text-center m-4">
+    <div className="hero min-h-screen min-w-full flex flex-col items-center justify-center">
+      <div className="hero-content text-center m-4">
         <div className="max-w-md">
-        <h1 className="text-5xl font-bold">Hi, I&apos;m Nuno—</h1>
-        <p className="text-3xl py-6 w-full">
-          a Software Engineer, book hobbyist, occassional community volunteer near London, England.
-        </p>
-        <p className="text-2xl py-3">
-          <span className="text-amber-200">Experienced in Python, C++, TypeScript, React, AWS Cloud, and more.</span>
-        </p>
+          <h1 className="text-5xl font-bold">Hi, I&apos;m Nuno—</h1>
+          <p className="text-3xl py-6 w-full">
+            a Software Engineer, book hobbyist, occasional community volunteer near London, England.
+          </p>
+          <p className="text-2xl py-3">
+            <span className="text-amber-200">Experienced in Python, C++, TypeScript, React, AWS Cloud, and more.</span>
+          </p>
         </div>
-    </div>
+      </div>
+      {/* Accordion Arrow Section */}
+      <div className="absolute bottom-10 flex justify-center animate-bounce">
+        <ArrowIcon width={25} height={25} />
+      </div>
     </div>
   )
 }
